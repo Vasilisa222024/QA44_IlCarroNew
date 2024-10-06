6686d2cb9e772d1eee36516ce8c4251a7b527bdf
Vasilisa222024 <155727090+Vasilisa222024@users.noreply.github.com>
2024-10-06 19:44:14 +0300

    start1.2

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -5,12 +5,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import utils.HeaderMenuItem;
 
 import java.time.Duration;
 
 public class BasePage {
     static WebDriver driver;
+    static Logger logger = LoggerFactory.getLogger(BasePage.class);
     public  static void  setDriver(WebDriver wd){
         driver=wd;
     }
